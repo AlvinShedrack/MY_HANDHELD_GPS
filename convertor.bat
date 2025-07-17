c181c730bbe87e977eb29de330e1dc630f521a93
AlvinShedrack <alvinshedrack90@gmail.com>
2025-07-17 19:17:45 +0300

    other mods

--- convertor.bat
+++ convertor.bat
@@ -1,17 +1,6 @@
 @echo off
-:: Set your commit message
-set /p message="Enter commit message: "
-
-:: Navigate to your project folder
-cd /d "D:\Cordinate Convertor"
-
-:: Add all changes
+cd /d D:\Cordinate Convertor
 git add .
-
-:: Commit with user message
-git commit -m "%message%"
-
-:: Push to GitHub
-git push origin main
-
+git commit -m "other mods"
+git push origin Main
 pause
